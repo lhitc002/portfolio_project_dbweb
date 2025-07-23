00cdd8d63affc33d8e95a876dd7eacdcb71ef93c
Bappity <12457072+bappitybup@users.noreply.github.com>
2025-07-23 11:31:33 +0100

    install express dependencies and add them to the app definition - bcrypt - express-mysql-session - express-sanitizer - express-session - express-validator

--- index.js
+++ index.js
@@ -5,11 +5,37 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
-const mysql = require('mysql2');
+const session = require('express-session');
+const expressSanitizer = require('express-sanitizer');
+const MySQLStore = require('express-mysql-session')(session);
 const loadRoutes = require('./routeLoader');
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Use express-session
+const sessionStore = new MySQLStore({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+// 1) mount express session
+app.use(session({
+    key: 'connect.sid',
+    secret: process.env.SESSION_SECRET || 'keyboard cat',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
+}));
+
+// 2) expose session to all views
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
 
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
@@ -19,36 +45,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 app.set('layout', 'shared/layout'); // refers to views/shared/layout.ejs
 
-// Set up the body parser 
-app.use(express.urlencoded({ extended: true }))
+// Set up the body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// express sanitizer
+app.use(expressSanitizer());
 
 // Set up public folder (for css and statis js)
-app.use(express.static(__dirname + '/public'))
-
-// Define the database connection
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'continuum_reader_app',
-    password: process.env.DB_PASS || 'qwertyuiop',
-    database: process.env.DB_NAME || 'continuum_reader',
-});
-
-// Connect to the database
-db.connect((err) => {
-    if (err) {
-        throw err
-    }
-    console.log('Connected to database')
-})
-global.db = db
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define our application-specific data
 app.locals.appData = {
-    appName: process.env.SHOP_NAME || 'Unnamed App'
-}
+    appName: process.env.APP_NAME || 'Unnamed App'
+};
 
 // Dynamically load the route handlers
 loadRoutes(app);
 
 // Start the web app listening
-app.listen(port, () => console.log(`Node app listening on port ${port}!`))
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
